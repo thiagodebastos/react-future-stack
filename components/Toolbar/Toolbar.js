c08d10b4a661f1a625668f3e4db96f9f89118cb7
Thiago de Bastos <thiago@thiagodebastos.com>
2017-06-16 14:44:12 +1000

    document bug regarding setting state on unmounted component

--- components/Toolbar/Toolbar.js
+++ components/Toolbar/Toolbar.js
@@ -14,6 +14,10 @@ class Toolbar extends Component {
     window.addEventListener('scroll', this.yScroll)
   }
 
+  /* BUG:
+    https://stackoverflow.com/a/40969739/3108412
+    do not call `setState` when component is unmounted
+  */
   yScroll = () => {
     if (window.pageYOffset <= 150 && !this.state.isExpanded) {
       this.setState({
