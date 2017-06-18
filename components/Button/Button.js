f31653a8b27ea3103ec5ab82d4b4307f0015bf22
Thiago de Bastos <thiago@thiagodebastos.com>
2017-06-18 18:41:41 +1000

    reformat to use semantic-ui button

--- components/Button/Button.js
+++ components/Button/Button.js
@@ -1,15 +1,22 @@
-import styled from 'styled-components'
+import styled from 'styled-components';
+import { Button as SemanticButton } from 'semantic-ui-react';
 
-const Button = styled.button`
+const Button = styled(SemanticButton)`
   cursor: pointer;
   fontSize: 1em;
   padding: .25em 1em;
   border-radius: 3px;
   margin: 5px;
   /* Theme Dependent Styles */
-  background: ${props => (props.primary ? props.theme.primary : props.theme.secondary) || 'white'};
-  color: ${props => (props.primary ? props.theme.secondary : props.theme.primary) || 'white'};
-  border: 2px solid ${props => (props.primary ? props.theme.primary : props.theme.primary) || 'palevioletred'};
-`
+  background: ${props =>
+    (props.primary ? props.theme.primary : props.theme.secondary) || 'white'};
 
-export default Button
+  color: ${props =>
+    (props.primary ? props.theme.secondary : props.theme.primary) || 'white'};
+
+  border: 2px solid ${props =>
+    (props.primary ? props.theme.primary : props.theme.primary) ||
+    'palevioletred'};
+`;
+
+export default Button;
